bec5a5ad5c76634c00ce2aa546f835ff9d4f00a8
Brunetto Ziosi <brunetto.ziosi@pixartprinting.com>
2016-12-21 18:43:34 +0100

    working

--- main.go
+++ main.go
@@ -1,47 +1,79 @@
 package main
 
 import (
-	"os/exec"
-	"log"
-	"io"
 	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
 	"regexp"
+	"time"
+
+	"github.com/brunetto/goutils/debug"
 )
 
-func main () {
+var Debug = false
+
+func main() {
 	var (
 		airportCmd *exec.Cmd
-		err error
-		stdout        io.ReadCloser
+		err        error
+		stdout     io.ReadCloser
 	)
 
+	fmt.Println("Scanning...")
 
-
+	debug.LogDebug(Debug, "Creating command")
 	airportCmd = exec.Command("airport", "-s")
-	stdout, err = airportCmd.StdoutPipe();
+
+	debug.LogDebug(Debug, "Setting STDOUT")
+	stdout, err = airportCmd.StdoutPipe()
 	if err != nil {
 		log.Fatal("Can't connect to mysqldump stdout.")
 	}
-	FilterStdout(stdout)
+
+	debug.LogDebug(Debug, "Setting STDERR")
+	airportCmd.Stderr = os.Stderr
+
+	debug.LogDebug(Debug, "Start command")
+	err = airportCmd.Start()
+	if err != nil {
+		log.Fatal("Can't start airport: ", err)
+	}
+
+	debug.LogDebug(Debug, "Start intercepting output and wait")
+	wil := FilterStdout(stdout)
 	err = airportCmd.Wait()
 	if err != nil {
 		log.Fatal("Wait dumping to finish: ", err)
 	}
+	debug.LogDebug(Debug, "Finished")
+
+	debug.LogDebug(Debug, "Print result")
+	fmt.Println()
+	for k, _ := range wil {
+		fmt.Println(k)
+	}
 }
 
 func FilterStdout(stdout io.ReadCloser) WinfoList {
+	if Debug {
+		defer debug.TimeMe(time.Now())
+	}
+
 	var (
 		scanner *bufio.Scanner
 		line    string
-		wil = WinfoList{}
-		reg = regexp.MustCompile(	`(?P<Name>\S+)\s+` +
-						`(?P<APMAC>([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))\s+` +
-						`(?P<Strength>-+\d+)\s+` +
-						`(?P<Channel>\d+)\s+` +
-						`(?P<HT>\S+)\s+` +
-						`(?P<CC>\S+)\s+` +
-						`(?P<Security>[A-Za-z0-9]+)` +
-						`(?P<SecurityGroup>\((?P<Auth>\S+)\/(?P<Unicast>\S+)\/(?P<Group>\S+)\)){0,1}`)
+		wil     = WinfoList{}
+		reg     = regexp.MustCompile(`(?P<Name>\S+)\s+` +
+			`(?P<APMAC>([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))\s+` +
+			`(?P<Strength>-+\d+)\s+` +
+			`(?P<Channel>\d+)\s+` +
+			`(?P<HT>\S+)\s+` +
+			`(?P<CC>\S+)\s+` +
+			`(?P<Security>[A-Za-z0-9]+)` +
+			`(?P<SecurityGroup>\((?P<Auth>\S+)\/(?P<Unicast>\S+)\/(?P<Group>\S+)\)){0,1}`)
 		match []string
 		names map[string]int
 	)
@@ -50,6 +82,10 @@ func FilterStdout(stdout io.ReadCloser) WinfoList {
 	for scanner.Scan() {
 		line = scanner.Text()
 		match = reg.FindStringSubmatch(line)
+		if len(match) == 0 {
+			continue
+		}
+
 		names = map[string]int{}
 		for i, n := range reg.SubexpNames() {
 			if i != 0 { // first name is empty because index 0 is the whole match
@@ -57,17 +93,21 @@ func FilterStdout(stdout io.ReadCloser) WinfoList {
 			}
 		}
 
-		wil = append(wil, Winfo{
-					Name: match[names["Name"]],
-					APMAC: match[names["APMAC"]],
-					Strength: match[names["Strength"]],
-					Channel: match[names["Channel"]],
-					HT: match[names["HT"]],
-					CC: match[names["CC"]],
-					Auth: match[names["Auth"]],
-					Unicast: match[names["Unicast"]],
-					Group: match[names["Group"]],
-					})
+		_, exists := wil[match[names["Name"]]]
+		if !exists {
+			wil[match[names["Name"]]] = []Winfo{}
+		}
+		wil[match[names["Name"]]] = append(wil[match[names["Name"]]], Winfo{
+			Name:     match[names["Name"]],
+			APMAC:    match[names["APMAC"]],
+			Strength: match[names["Strength"]],
+			Channel:  match[names["Channel"]],
+			HT:       match[names["HT"]],
+			CC:       match[names["CC"]],
+			Auth:     match[names["Auth"]],
+			Unicast:  match[names["Unicast"]],
+			Group:    match[names["Group"]],
+		})
 
 	}
 
@@ -75,16 +115,16 @@ func FilterStdout(stdout io.ReadCloser) WinfoList {
 }
 
 type Winfo struct {
-	Name string  // (E)SSID
-	APMAC string // BSSID
+	Name     string // (E)SSID
+	APMAC    string // BSSID
 	Strength string // RSSI
-	Channel string
-	HT string
-	CC string
+	Channel  string
+	HT       string
+	CC       string
 	Security string
-	Auth string
-	Unicast string
-	Group string
+	Auth     string
+	Unicast  string
+	Group    string
 }
 
 type WinfoList map[string][]Winfo
